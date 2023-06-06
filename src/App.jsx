7fff608d70c77a7df72decd28bc704db104c2130
Luis Matos <luismatos138@gmail.com>
2023-06-06 17:48:25 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,71 @@
 import React, { useState } from "react";
+import WarningMessage from "./components/WarningMessage";
+import ErrorMessage from "./components/ErrorMessage";
+import SuccessMessage from "./components/SuccessMessage";
 
 function App() {
   const [text, setText] = useState("");
-
+  const [checkIfNumbers, setCheckIfNumbers] = useState(false);
   const handleChange = (event) => {
-    setText(event.target.value);
+    const value = event.target.value;
+  const noSpaceText = removeSpaces(value);
+  const hasNumbers = hasNumber(noSpaceText);
+  setCheckIfNumbers(hasNumbers);
+  setText(noSpaceText);
+};
+const squareSize = text.length*10;
+const handleReset = (event) => {
+  const value = event.target.value;
+  setText("")
+};
+const handleUppercase = (event) => {
+  const value = event.target.value;
+  setText(text.toUpperCase())
+}
+  function hasNumber(value) {
+    return /\d/.test(value);
+  }
+  const removeSpaces = (value) => {
+    return value.replaceAll(" ", "");
   };
 
+  
   return (
-    <div>
+       <div>
       <h1 class="title">Character counter</h1>
       <main className="main">
+        
+
         <input
+          style={{ color: text.length > 10 ? "green" : "inherit" }}
           className="textInput"
           placeholder="Enter a word"
           value={text}
           onChange={handleChange}
         />
+        {text.length === 0 ? <WarningMessage /> : <SuccessMessage/> && checkIfNumbers ? <ErrorMessage /> : <SuccessMessage/>}
+        
+
         <p className="word">
-          <b>Word:</b> {text}
+          <b>Word: {text}</b>
         </p>
         <p>Number of characters: {text.length}</p>
+        <button onClick={handleReset}> Reset </button>
+        <button onClick={handleUppercase}> Uppercase</button>
+
+        <div className="square" style={{
+            width: `${squareSize}px`,
+            height: `${squareSize}px`,
+            backgroundColor: "black"
+          }} >
+       
+          
+    </div>
       </main>
     </div>
+  
+   
+
   );
 }
 
---
+++ src/components/SuccessMessage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function SucessMessage() {
+  return (
+    <div className="successMessage">
+      <span role="img" aria-label="Sucessfull">
+       
+      </span>{" "}
+      <b>Success</b>
+      <br />
+      Its a valid word
+    </div>
+  );
+}
+
+export default SucessMessage;
